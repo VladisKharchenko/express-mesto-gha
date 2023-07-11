44c875a504e38b6294ad5c428df33531d5f46119
Владислав Харченко <120054604+VladisKharchenko@users.noreply.github.com>
2023-07-11 23:32:21 +0400

    Работа от 11.07.2023-9

--- controllers/users.js
+++ controllers/users.js
@@ -1,15 +1,13 @@
 const User = require('../models/user');
 
 const ERROR_CODE = 400;
-const ERROR_CODE_2 = 500;
-const ERROR_CODE_3 = 404;
 
 const getUsers = async (req, res) => {
   try {
     const users = await User.find();
     res.json(users);
   } catch (error) {
-    res.status(ERROR_CODE_2).json({ error: 'На сервере произошла ошибка' });
+    res.status(500).json({ error: 'На сервере произошла ошибка' });
   }
 };
 
@@ -17,28 +15,32 @@ const getUserById = async (req, res) => {
   const { userId } = req.params;
   try {
     const user = await User.findById(userId);
-    if (!user) {
-      return res.status(ERROR_CODE_3).json({ error: 'Пользователь по указанному _id не найден' });
-    }
     res.json(user);
   } catch (error) {
-    res.status(ERROR_CODE_2).json({ error: 'На сервере произошла ошибка' });
+    if (error.name === 'CastError') {
+      return res
+        .status(ERROR_CODE)
+        .json({ message: 'Пользователь по указанному _id не найден' });
+    }
+    res.status(500).json({ error: 'На сервере произошла ошибка' });
   }
   return res;
 };
 
-const createUser = (req, res) => {
+const createUser = async (req, res) => {
   const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
-    .then((user) => {
-      res.status(201).json(user);
-    })
-    .catch((error) => {
-      if (error.name === 'ValidationError') {
-        return res.status(ERROR_CODE).json({ message: 'Переданы некорректные данные при создании пользователя' });
-      }
-      return res.status(500).json({ error: 'На сервере произошла ошибка' });
-    });
+  try {
+    const user = await User.create({ name, about, avatar });
+    res.status(201).json(user);
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      return res.status(ERROR_CODE).json({
+        message: 'Переданы некорректные данные при создании пользователя',
+      });
+    }
+    return res.status(500).json({ error: 'На сервере произошла ошибка' });
+  }
+  return res;
 };
 
 const updateUserProfile = async (req, res) => {
@@ -49,15 +51,19 @@ const updateUserProfile = async (req, res) => {
       { name, about },
       { new: true },
     );
-    if (!user) {
-      return res.status(ERROR_CODE_3).json({ error: 'Пользователь с указанным _id не найден' });
-    }
     res.json(user);
   } catch (error) {
-    if (error.name === 'SomeErrorName') {
-      return res.status(ERROR_CODE).json({ error: 'Переданы некорректные данные при обновлении профиля' });
+    if (error.name === 'ValidationError') {
+      return res
+        .status(ERROR_CODE)
+        .json({ message: 'Переданы некорректные данные при обновлении профиля' });
     }
-    res.status(ERROR_CODE_2).json({ error: 'На сервере произошла ошибка' });
+    if (error.name === 'CastError') {
+      return res
+        .status(404)
+        .json({ message: 'Пользователь с указанным _id не найден' });
+    }
+    res.status(500).json({ error: 'На сервере произошла ошибка' });
   }
   return res;
 };
@@ -66,20 +72,24 @@ const updateUserAvatar = async (req, res) => {
   const { avatar } = req.body;
   try {
     if (!avatar) {
-      return res.status(ERROR_CODE).json({ error: 'Переданы некорректные данные при обновлении аватара' });
+      return res
+        .status(ERROR_CODE)
+        .json({ error: "Переданы некорректные данные при обновлении аватара" });
     }
 
     const user = await User.findByIdAndUpdate(
       req.user._id,
       { avatar },
-      { new: true },
+      { new: true }
     );
     if (!user) {
-      return res.status(ERROR_CODE_3).json({ error: 'Пользователь с указанным _id не найден' });
+      return res
+        .status(ERROR_CODE_3)
+        .json({ error: "Пользователь с указанным _id не найден" });
     }
     res.json(user);
   } catch (error) {
-    res.status(ERROR_CODE_2).json({ error: 'На сервере произошла ошибка' });
+    res.status(ERROR_CODE_2).json({ error: "На сервере произошла ошибка" });
   }
   return res;
 };
