8361ddc3a34c68af128d671c7833e5776fdafad2
Владислав Харченко <120054604+VladisKharchenko@users.noreply.github.com>
2023-07-11 15:35:01 +0400

    Работа от 11.07.2023-1

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,6 @@
 const User = require('../models/user');
 
-const ERROR_CODE_1 = 400;
+const ERROR_CODE = 400;
 const ERROR_CODE_2 = 500;
 const ERROR_CODE_3 = 404;
 
@@ -33,8 +33,12 @@ const createUser = async (req, res) => {
     const user = await User.create({ name, about, avatar });
     res.status(201).json(user);
   } catch (error) {
-    res.status(ERROR_CODE_1).json({ error: 'Переданы некорректные данные при создание пользователя' });
+    if (error.name === 'SomeErrorName') {
+      return res.status(ERROR_CODE).json({ error: 'Переданы некорректные данные при создание пользователя' });
+    }
+    res.status(500).json({ error: 'На сервере произошла ошибка' });
   }
+  return res;
 };
 
 const updateUserProfile = async (req, res) => {
@@ -51,7 +55,7 @@ const updateUserProfile = async (req, res) => {
     res.json(user);
   } catch (error) {
     if (error.name === 'SomeErrorName') {
-      return res.status(ERROR_CODE_1).json({ error: 'Переданы некорректные данные при обновлении профиля' });
+      return res.status(ERROR_CODE).json({ error: 'Переданы некорректные данные при обновлении профиля' });
     }
     res.status(ERROR_CODE_2).json({ error: 'На сервере произошла ошибка' });
   }
@@ -62,7 +66,7 @@ const updateUserAvatar = async (req, res) => {
   const { avatar } = req.body;
   try {
     if (!avatar) {
-      return res.status(ERROR_CODE_1).json({ error: 'Переданы некорректные данные при обновлении аватара' });
+      return res.status(ERROR_CODE).json({ error: 'Переданы некорректные данные при обновлении аватара' });
     }
 
     const user = await User.findByIdAndUpdate(
