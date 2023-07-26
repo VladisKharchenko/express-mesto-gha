b5f6b0b8e64945c4e88a53c456ad881ef9fca49f
Владислав Харченко <120054604+VladisKharchenko@users.noreply.github.com>
2023-07-26 23:44:52 +0400

    Работа от 26.07.2023-13

--- app.js
+++ app.js
@@ -29,10 +29,10 @@ app.post(
     body: Joi.object()
       .keys({
         name: Joi.string().min(2).max(30),
-        about: Joi.string().min(2).max(30).required(),
+        about: Joi.string().min(2).max(30),
         email: Joi.string().email().required(),
         password: Joi.string().min(8).required(),
-        avatar: Joi.string().uri().required(),
+        avatar: Joi.string().uri(),
       })
       .unknown(true),
   }),
